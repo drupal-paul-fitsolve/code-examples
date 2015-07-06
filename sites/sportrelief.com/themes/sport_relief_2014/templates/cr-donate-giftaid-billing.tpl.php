96903cb918774163648e6edf07bb8a1e1bffb6dd
Paul Wilkin <P.Wilkin@comicrelief.com>
2015-07-06 18:28:29 +0100

    removed sites!

--- sites/sportrelief.com/themes/sport_relief_2014/templates/cr-donate-giftaid-billing.tpl.php
+++
@@ -1,9 +0,0 @@
-<div class="cr-donate-row form-row clearfix">
-  <div class="grid-8 narrow-grid-10 alpha narrow-alpha"><?php print drupal_render($form['title']); ?></div>
-</div>
-<div class="cr-donate-row form-row clearfix">
-  <div class="grid-8 narrow-grid-10 alpha narrow-alpha"><?php print drupal_render($form['first_name']); ?></div>
-  <div class="grid-8 narrow-grid-10 omega narrow-omega"><?php print drupal_render($form['last_name']); ?></div>
-</div>
-
-<?php print drupal_render_children($form); ?>
