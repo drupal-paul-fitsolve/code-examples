6153df874c150b30504325afe7353f3437a09bf1
Paul Wilkin <p.wilkin@comicrelief.com>
2015-07-14 16:40:54 +0100

    removed others work

--- profiles/crdistro/modules/custom/all/quickfinder/quickfinder-block.tpl.php
+++
@@ -1,7 +0,0 @@
-<div class="quickfinder-wrapper">
-	<input type="text" class="quickfinder-box text-input">
-</div>
-
-<!--<div class="quickfinder-select-wrapper">
-	<select class="quickfinder-select form-select"></select>
-</div>-->
